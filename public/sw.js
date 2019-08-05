1a3c787f6266be3d4d510b9825de83b58246be1f
Hancock <bhancock8@elsys.gtri.org>
2019-08-05 12:36:09 -0400

    added very basic dynamic caching

--- public/sw.js
+++ public/sw.js
@@ -1,8 +1,11 @@
+var CACHE_STATIC_NAME = 'static-v4';
+var CACHE_DYNAMIC_NAME = 'dynamic-v2';
+
 // Triggered by the browser
 self.addEventListener('install', function(event){
     console.log('[Service Worker] Installing Service Worker ...', event);
     event.waitUntil(
-        caches.open('static')
+        caches.open(CACHE_STATIC_NAME)
             .then(function(cache){
                 console.log('[Service Worker] Precaching app shell')
                 // Caching URLS
@@ -27,8 +30,20 @@ self.addEventListener('install', function(event){
 
 // If you have a tab open, it will register new SW but not activate it.
 // Triggered by the browser
+// Only triggered when user closes all tables and opens new window
 self.addEventListener('activate', function(event){
     console.log('[Service Worker] Activating Service Worker ...', event);
+    event.waitUntil(
+        caches.keys()
+            .then(function(keyList){
+                return Promise.all(keyList.map(function(key){
+                    if(key !== CACHE_STATIC_NAME && key !== CACHE_DYNAMIC_NAME){
+                        console.log('[Service Worker] Removing old cache.', key);
+                        return caches.delete(key);
+                    }
+                }))
+            })
+    )
     return self.clients.claim();
 });
 
@@ -40,7 +55,17 @@ self.addEventListener('fetch', function(event){
                 if(response){
                     return response;
                 } else {
-                    return fetch(event.request);
+                    return fetch(event.request)
+                        .then(function(res){
+                            caches.open(CACHE_DYNAMIC_NAME)
+                                .then(function(cache){
+                                    cache.put(event.request.url, res.clone());
+                                    return res;
+                                })
+                        })
+                        .catch(function(err){
+                            console.log(err);
+                        });
                 }
             })
             .catch(function(err){
