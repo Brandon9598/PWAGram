83666f14e8e7a730144888654d5956b5cd33148d
Brandon Hancock <Brandon.Hancock@gtri.gatech.edu>
2019-08-05 10:51:09 -0400

    init commit

---
+++ public/sw.js
@@ -0,0 +1,17 @@
+// Triggered by the browser
+self.addEventListener('install', function(event){
+    console.log('[Service Worker] Installing Service Worker ...', event);
+});
+
+// If you have a tab open, it will register new SW but not activate it.
+// Triggered by the browser
+self.addEventListener('activate', function(event){
+    console.log('[Service Worker] Activating Service Worker ...', event);
+    return self.clients.claim();
+});
+
+//  Triggered by the web application
+self.addEventListener('fetch', function(event){
+    console.log('[Service Worker] Fetching something', event);
+    event.respondWith(fetch(event.request));
+});
