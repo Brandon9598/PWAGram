2ea48e26a172ad1f99fff9d8fd2b1ac6ab75aa72
Hancock <bhancock8@elsys.gtri.org>
2019-08-05 12:12:22 -0400

    add static caching

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,28 @@
 // Triggered by the browser
 self.addEventListener('install', function(event){
     console.log('[Service Worker] Installing Service Worker ...', event);
+    event.waitUntil(
+        caches.open('static')
+            .then(function(cache){
+                console.log('[Service Worker] Precaching app shell')
+                // Caching URLS
+                cache.addAll([
+                    '/',
+                    '/index.html',
+                    '/src/js/app.js',
+                    '/src/js/feed.js',
+                    '/src/js/promise.js',
+                    '/src/js/fetch.js',
+                    '/src/js/material.min.js',
+                    '/src/css/app.css',
+                    '/src/css/feed.css',
+                    '/src/images/main-image.jpg',
+                    'https://fonts.googleapis.com/css?family=Roboto:400,700',
+                    'https://fonts.googleapis.com/icon?family=Material+Icons',
+                    'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css'
+                ]);
+            })
+    );
 });
 
 // If you have a tab open, it will register new SW but not activate it.
@@ -12,5 +34,17 @@ self.addEventListener('activate', function(event){
 
 //  Triggered by the web application
 self.addEventListener('fetch', function(event){
-    event.respondWith(fetch(event.request));
+    event.respondWith(
+        caches.match(event.request)
+            .then(function(response){
+                if(response){
+                    return response;
+                } else {
+                    return fetch(event.request);
+                }
+            })
+            .catch(function(err){
+                console.log(err);
+            })
+    );
 });
