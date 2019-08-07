b293d8de7580e040cdc49e04dd2b270b85a94485
Hancock <bhancock8@elsys.gtri.org>
2019-08-07 14:29:45 -0400

    added options for notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -24,13 +24,32 @@ window.addEventListener('beforeinstallprompt', function(event) {
   return false;
 });
 
+function displayConfirmationNotification(){
+  if('serviceWorker' in navigator){
+    var options = {
+      body: 'You successfully subscribed to our Notification service',
+      icon: '/src/images/icons/app-icon-96x96.png',
+      image: '/src/images/sf-boat.jpg',
+      dir: 'ltr',
+      lang: 'en-US', // BCP 47
+      vibrate: [100, 50, 200], // Vibration pause vibration
+      badge: '/src/images/icons/app-icon-96x96.png'
+    };
+
+    navigator.serviceWorker.ready
+      .then(function(swreg){
+        swreg.showNotification('Succesffuly subscribed (from sw)', options);
+      })
+  } 
+}
+
 function askForNotificationPermission(){
   Notification.requestPermission(function(result){
     console.log('User choice', result);
     if(result !== 'granted'){
       console.log('No notification permissions granted');
     } else {
-      
+      displayConfirmationNotification();
     }
   });
 }
